64067200743496fdfb44b92df7f959b972e355fb
Carlos Mullov <mullovc@hotmail.de>
2017-09-23 07:49:49 +0200

    aded training, player index to network input, fixed bugs

--- learn_chess.py
+++ learn_chess.py
@@ -8,14 +8,15 @@ import tensorflow as tf
 import main as chess
 
 def loss(args):
-    board_batch = args[:,   :128].reshape(-1, 8, 8, 2)
-    fro_batch   = args[:,128:130].astype(np.int32)
-    to_batch    = args[:,130:132].astype(np.int32)
+    board_batch  = args[:,   :128].reshape(-1, 8, 8, 2)
+    fro_batch    = args[:,128:130].astype(np.int32)
+    to_batch     = args[:,130:132].astype(np.int32)
+    player_batch = args[:,    133].astype(np.int32)
 
     batch_loss = []
-    for board, fro, to in zip(board_batch, fro_batch, to_batch):
+    for board, fro, to, player in zip(board_batch, fro_batch, to_batch, player_batch):
         occ = board[to[0], to[1], 1] != 0
-        moved, check = chess.move(board, fro, to)
+        moved, check = chess.move(board, fro, to, player)
         if check:
             batch_loss += [0]
             continue
@@ -35,11 +36,11 @@ def loss(args):
         #return np.array([120 - valid * 50 - beat * 50 + obst * 50])
     return np.array(batch_loss).astype(np.float32)
 
-def reward(board_flat, fro, to):
+def reward(board_flat, fro, to, player):
     board = board_flat.reshape(8, 8, 2)
 
     occ = board[to[0], to[1], 1] != 0
-    moved, check = chess.move(board, fro, to)
+    moved, check = chess.move(board, fro, to, player)
     if check:
         return 0
     if moved:
@@ -54,7 +55,7 @@ def reward(board_flat, fro, to):
 
     return 120 - valid * 50 - beat * 50 + obst * 50 + same * 50
 
-def play(a, Qmax, sess, actions):
+def play(Qmax, inputs, actions, sess):
     board = chess.build_board()
     board_flat = board.flatten()
     won = 0
@@ -62,47 +63,75 @@ def play(a, Qmax, sess, actions):
     actions_rewards = []
 
     while not won:
-        for player in xrange(1, 3):
-            suc = False
-            while not suc:
-                aidx, estim = sess.run([a, Qmax], { state : board_flat })
-                fro, to = actions[aidx].reshape((2, 2))
-                suc, check = chess.move(board, fro, to)
-                r = reward(board_flat, fro, to)
-                actions_rewards.append((np.copy(board_flat), estim, r))
+        for p in xrange(1, 3):
+            (estim, aidx), inp = sess.run([Qmax, inputs], { state : board_flat, player : [p] })
+            for i, a in zip(aidx.flatten(), estim.flatten()):
+                fro, to = actions[i].reshape((2, 2))
+                suc, check = chess.move(board, fro, to, p)
+                #print suc, check
+                r = reward(board_flat, fro, to, p)
+                actions_rewards.append((np.copy(inp[i]), a, r))
+                if suc:
+                    break
+            if not suc:
+                print "Couldn't find suitable move in top 8192 candidates"
+                won = 3
+
+            chess.print_highlight_move(board, fro, to)
 
             if check:
-                won = player
+                won = p
                 break
 
     print "Player " + str(won) + " has won!"
     return actions_rewards
+
+def save_model(name, W, b, sess):
+    Wz, bz = sess.run([W, b])
+    np.savez('models/' + name, W=Wz, b=bz)
 
 if __name__ == '__main__':
     board = chess.build_board()
     flat_board = board.flatten()
     batch_size = 1
     #out_shape = (4,)
-    in_shape = (flat_board.shape[0] + 4,)
+    in_shape = (flat_board.shape[0] + 5,)
     out_shape = (1,)
 
-    W = tf.random_normal(in_shape + out_shape)
-    b = tf.random_normal((1,) + out_shape)
+    W = tf.Variable(tf.random_normal(in_shape + out_shape))
+    b = tf.Variable(tf.random_normal((1,) + out_shape))
 
-    #x = tf.placeholder(tf.float32, shape=(None,) + in_shape)
-    #o = tf.matmul(x, W) + b
 
-    state = tf.placeholder(tf.float32, flat_board.shape)
+    player = tf.placeholder(tf.float32, [1])
+    state  = tf.placeholder(tf.float32, flat_board.shape)
     actions = np.array(list(product(xrange(8), repeat=4)))
-    inputs = tf.concat([tf.tile(tf.reshape(state, (1, -1)), [len(actions), 1]), actions], axis=1)
+    tiled_state  = tf.tile(tf.reshape(state,  (1, -1)), [len(actions), 1])
+    tiled_player = tf.tile(tf.reshape(player, (1, -1)), [len(actions), 1])
+    inputs = tf.concat([tiled_state, tiled_player, actions], axis=1)
     Qvals = tf.matmul(inputs, W) + b
-    a = tf.cast(tf.argmax(Qvals, axis=0)[0], tf.int32)
-    Qmax = Qvals[a]
+    #a = tf.cast(tf.argmax(Qvals, axis=0)[0], tf.int32)
+    #Qmax = Qvals[a]
+    Qmax = tf.nn.top_k(tf.transpose(Qvals), k=8**4)
     
+
     #r = tf.py_func(loss, [x], tf.float32, stateful=False)
+    x = tf.placeholder(tf.float32, shape=(None,) + in_shape)
+    t = tf.placeholder(tf.float32, shape=(None,) + out_shape)
+    o = tf.matmul(x, W) + b
+    E = tf.reduce_sum(tf.square(t - o))
+
+    train = tf.train.GradientDescentOptimizer(0.5).minimize(E)
 
     init = tf.global_variables_initializer()
     sess = tf.Session()
     sess.run(init)
 
-    ac_re = play(a, Qmax, sess, actions)
+    for i in xrange(1):
+        acre = play(Qmax, inputs, actions, sess)
+
+        states = np.array([s for s, _, _ in acre])
+        estims = np.array([e for _, e, _ in acre])
+        labels = np.array([l for _, _, l in acre]).reshape(-1, 1)
+        sess.run(train, { x : states, t : labels })
+
+    save_model('model', W, b, sess)
