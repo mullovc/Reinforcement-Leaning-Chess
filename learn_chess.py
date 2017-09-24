f18ffa1ed32dd6678bd1e0bff4394656ce51b288
Carlos Mullov <mullovc@hotmail.de>
2017-09-24 13:45:06 +0200

    experimenting with activation and reward functions
    - reward now between 0 and 1, with sigmoid as output activation

--- learn_chess.py
+++ learn_chess.py
@@ -12,14 +12,14 @@ from regressor import Regressor
 
 def calc_reward(log, won, rho):
     if   won ==  1:
-        p1init_rew =  1
-        p2init_rew = -1
+        p1init_rew = 1
+        p2init_rew = 0.3
     elif won == -1:
-        p1init_rew = -1
-        p2init_rew =  1
+        p1init_rew = 0.3
+        p2init_rew = 1
     elif won == 3:
-        p1init_rew = -1
-        p2init_rew = -1
+        p1init_rew = 0.3
+        p2init_rew = 0.3
     p1log = [(i, x) for (i, x) in enumerate(log) if x[0] ==  1]
     p2log = [(i, x) for (i, x) in enumerate(log) if x[0] == -1]
 
@@ -46,17 +46,22 @@ def norec_reward(init_rval, log, rho):
         c_rval = rval
         if suc:
             rval = rval * rho
-        else:
-            rval = rval
 
         if check:
-            c_rval += 0.6
+            c_rval = 1
         elif suc and occ:
-            c_rval += 0.4
+            c_rval = 0.6
         elif suc:
-            c_rval += 0.15
+            c_rval = 0.4
         else:
-            c_rval += valid*0.1 + beat*0.15 - same*0.4 - obst*0.3 
+            if not valid:
+                c_rval *= 0.3
+            if not beat:
+                c_rval *= 0.8
+            if same:
+                c_rval *= 0.3
+            if obst:
+                c_rval *= 0.4
         rew.append((i, c_rval))
     return rew
 
@@ -65,7 +70,7 @@ def play(regr):
     board_flat = board.reshape((-1))
     won = 0
 
-    k = 8**3
+    k = 16
     actions = []
     reward_log = []
     round_counter = 0
@@ -111,26 +116,28 @@ def play(regr):
 
 def print_state(state):
     board = state[:128].reshape(8,8,2)
-    #player = state[128]
+    player = 'black' if all(state[128:130] == [1, 0]) else 'white'
+    
     f, t = np.argmax(state[130:].reshape((2, 64)), axis=1)
     fro = (f / 8, f % 8)
     to  = (t / 8, t % 8)
+    print player
     chess.print_highlight_move(board, fro, to)
 
 def train_with_transcript(regr):
     f = np.load('data/transcript.npz')
     transcript_states = regr.gen_states(f['boards'], f['players'], f['froms'], f['tos'])
     transcript_labels = f['rewards'].reshape(-1, 1)
-    for i in xrange(100):
+    for i in xrange(10):
         e = regr.train_one_match(transcript_states, transcript_labels)
         print "Match " + str(i) + ":\n" + "Error: " +  str(e) + "\tinstances in last match: " + str(transcript_labels.shape[0])
-    regr.save('models/transcript_model.npz')
+    #regr.save('models/transcript_model.npz')
 
 def train_mixed(regr):
     f = np.load('data/transcript.npz')
     transcript_states = regr.gen_states(f['boards'], f['players'], f['froms'], f['tos'])
     transcript_labels = f['rewards'].reshape(-1, 1)
-    for i in xrange(20):
+    for i in xrange(100):
         states, rew, logs = play(regr)
         labels = np.array(rew).reshape(-1, 1)
         st = np.concatenate([states, transcript_states])
@@ -140,13 +147,15 @@ def train_mixed(regr):
 
 
 if __name__ == '__main__':
-    regr = Regressor((265,), (tf.nn.sigmoid, tf.identity))
+    regr = Regressor((258,128), (tf.nn.tanh, tf.nn.sigmoid))
 
-    train_with_transcript(regr)
-    exit()
-
-    for i in xrange(20):
-        states, rew, logs = play(regr)
-        labels = np.array(rew).reshape(-1, 1)
-        e = regr.train_one_match(states, labels)
-        print "Match " + str(i) + ":\n" + "Error: " +  str(e) + "\tinstances in last match: " + str(labels.shape[0])
+    train_mixed(regr)
+    regr.save('models/2hl_mixed_tr.npz')
+    
+    #for _ in xrange(5):
+    #    train_with_transcript(regr)
+    #    for i in xrange(10):
+    #        states, rew, logs = play(regr)
+    #        labels = np.array(rew).reshape(-1, 1)
+    #        e = regr.train_one_match(states, labels)
+    #        print "Match " + str(i) + ":\n" + "Error: " +  str(e) + "\tinstances in last match: " + str(labels.shape[0])
